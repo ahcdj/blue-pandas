86f3d72d7244e4deaf19c56704c787c51387cfff
Hoonio <hoonio@hoonio.com>
2016-03-20 19:25:39 +0000

    1-2 data slicing for multiple stocks

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 """Plot High prices for IBM"""
 
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -12,19 +13,57 @@ def get_mean_volume(symbol):
     # TODO: Compute and return the mean volume for this stock
     return df['Volume'].mean()
 
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol"""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files """
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        df_temp=pd.read_csv(symbol_to_path(symbol), index_col="Date", parse_dates=True, usecols=['Date','Adj Close'], na_values=['nan'])
+        df_temp=df_temp.rename(columns={'Adj Close':symbol})
+        df=df.join(df_temp,how='inner')
+        if symbol == 'SPY':  # drop dates SPY did not trade
+            df = df.dropna(subset=["SPY"])
+    # reverse to incrementing date order
+    return df.iloc[::-1]
+
+def plot_data(df,title="Stock prices"):
+    '''Plot stock prices'''
+    ax = normalize_data(df).plot(title=title)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Price")
+    plt.show()
+
+def plot_selected(df, selected_stocks, start_date, end_date):
+    plot_data(df.ix[start_date:end_date, selected_stocks])
+
+def normalize_data(df):
+    '''Normalize stock prices using the first row of the dataframe'''
+    return df/df.ix[0,:]
+
 def test_run():
     #"""Function called by Test Run."""
     #for symbol in ['AAPL', 'IBM']:
         #print "Mean Volume"
         #print symbol, get_mean_volume(symbol)
 
-    df = pd.read_csv("data/IBM.csv")
+    # df = pd.read_csv("data/IBM.csv")
     #print df.head(5)
     #return df['Volume'].mean()
 
-    df['High'].plot()
-    plt.show()  # must be called to show plots
+    dates=pd.date_range('2010-01-01', '2010-12-31')
+    symbols =['GOOG','IBM','GLD']
 
+    df1 = get_data(symbols, dates)
+    # print df1.ix['2010-03-01':'2010-04-01', ['SPY','IBM']]
+    # print df1
+    # plot_data(df1)
+    plot_selected(df1, ['SPY', 'IBM'], '2010-03-01', '2010-04-01')
 
 if __name__ == "__main__":
     test_run()
